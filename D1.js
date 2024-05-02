9a5ba5e30264b4540e56837e6eed9da99e1cbf09
kim-gf <68000305+kim-gf@users.noreply.github.com>
2024-05-02 10:10:46 +0300

    Add files via upload

---
+++ D1.js
@@ -0,0 +1,9 @@
+function createHelloWorld() {
+  return function () {
+    return "Hello World";
+  };
+}
+
+// Example usage:
+const helloWorldFunction = createHelloWorld();
+console.log(helloWorldFunction()); // Output: "Hello World"
